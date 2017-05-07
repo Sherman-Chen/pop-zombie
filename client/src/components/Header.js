0c9379181f9a1c7d0b631f2ac36ed37627686fd2
Sherman <shc410@gmail.com>
2017-05-07 13:07:52 -0700

    create src folder, Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+// @flow
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+  return (
+    <Text>Albums!</Text>
+  )
+};
+
+export default Header;
