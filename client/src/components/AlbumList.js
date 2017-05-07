3908b814d305ffb4372587d5b3fa68bdbc9c1f47
Sherman <shc410@gmail.com>
2017-05-07 14:27:49 -0700

    import axios, fetch data from api endpoint, setState to response

--- client/src/components/AlbumList.js
+++ client/src/components/AlbumList.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 export default class AlbumList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      albums: []
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    try {
+      let response = await axios.get('https://rallycoding.herokuapp.com/api/music_albums');
+      let albums = response.data;
+
+      this.setState({
+        albums
+      }, () => {console.log(this.state.albums)});
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   render() {
     return (
       <View>
@@ -9,4 +35,4 @@ export default class AlbumList extends Component {
       </View>
     )
   }
-};
+}
