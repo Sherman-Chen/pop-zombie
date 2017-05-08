808ff6eaf26fe3b66c98b27b861f9f321d2400e9
Sherman <shc410@gmail.com>
2017-05-08 02:47:41 -0700

    finish mock flipcard, create JSON later and map it appropriately

--- client/src/components/TrackListingDetail.js
+++ client/src/components/TrackListingDetail.js
@@ -8,20 +8,17 @@ export default class TrackListingDetail extends Component {
 
     return (
       <View style={TrackContainer}>
-        <Text>1</Text>
-        <Text>2</Text>
-        <Text>3</Text>
-        <Text>4</Text>
-        <Text>5</Text>
-        <Text>6</Text>
-        <Text>7</Text>
-        <Text>8</Text>
-        <Text>9</Text>
-        <Text>10</Text>
-        <Text>11</Text>
-        <Text>12</Text>
-        <Text>13</Text>
-        <Text>14</Text>
+        <Text>1. "Tim McGraw" - 3:54</Text>
+        <Text>2. "Picture to Burn" - 2:55</Text>
+        <Text>3. "Teardrops on My Guitar" - 3:35</Text>
+        <Text>4. "A Place in This World" - 3:22</Text>
+        <Text>5. "Cold as You" - 4:01</Text>
+        <Text>6. "The Outside" - 3:29</Text>
+        <Text>7. "TiedTogether with a Smile" - 4:11</Text>
+        <Text>8. "Stay Beautiful" - 3:58</Text>
+        <Text>9. "Should've Said No" - 4:04</Text>
+        <Text>10. "Mary's Song (Oh My My My)" - 3:35</Text>
+        <Text>11. "Our Song" - 3:24</Text>
       </View>
     )
   }
@@ -30,7 +27,10 @@ export default class TrackListingDetail extends Component {
 const styles = StyleSheet.create({
   TrackContainer: {
     height: '100%',
+    flex: 1,
     justifyContent: 'space-around',
-    padding: 5,
+    padding: 10,
+    borderColor: '#dddddd',
+    borderBottomWidth: 1
   }
 });
